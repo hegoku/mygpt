6d4a8ebde38e874269ef447916079d3e6e716ccc
Jack <hegokujack@gmail.com>
2025-06-13 10:47:47 +0800

    train wiki

--- DeepseekTokenizer.py
+++ DeepseekTokenizer.py
@@ -3,9 +3,11 @@ from transformers import AutoTokenizer
 class DeepseekTokenizer():
     def __init__(self):
         self.tokenizer = AutoTokenizer.from_pretrained("deepseek_tokenizer")
+        self.bos_token_id = self.tokenizer.bos_token_id
+        self.eos_token_id = self.tokenizer.eos_token_id
 
     def encode(self, text:str):
-        return self.tokenizer.encode(text, add_special_tokens=False, padding_side='right', return_tensors='pt')[0]
+        return self.tokenizer.encode(text, padding_side='right', return_tensors='pt')[0]
     
     def decode(self, ids) -> str:
         return self.tokenizer.decode(ids)
--- MyDataset.py
+++ MyDataset.py
@@ -40,3 +40,49 @@ def create_dataloader_v1(txt, tokenizer, batch_size=4, max_length=256,
     )
 
     return dataloader
+
+class MyDataset2(Dataset):
+    def __init__(self, txt_d, tokenizer, max_length, stride):
+        self.input_ids = []
+        self.target_ids = []
+
+        for txt in txt_d:
+            i = 0
+            token_ids = tokenizer.encode(txt['text'])
+            remaining_size = token_ids.shape[0]
+            while remaining_size>0:
+                if (remaining_size<=max_length):
+                    input_chunk = token_ids[i:]
+                    target_chunk = token_ids[i + 1: ]
+                # if (i+max_length+1) >= token_ids.shape[0]:
+                #     input_chunk = token_ids[i:i + max_length]
+                #     target_chunk = token_ids[i + 1: i + max_length + 1]
+                    # tmp = torch.full([max_length-input_chunk.shape[0]], tokenizer.eos_token_id)
+                    # input_chunk = torch.cat((input_chunk, tmp))
+                    # tmp = torch.full([max_length-target_chunk.shape[0]], tokenizer.eos_token_id)
+                    # target_chunk = torch.cat((target_chunk, tmp))
+                    tmp = torch.full([max_length-input_chunk.shape[0]], tokenizer.eos_token_id)
+                    input_chunk = torch.cat((input_chunk, tmp))
+                    tmp = torch.full([max_length-target_chunk.shape[0]], tokenizer.eos_token_id)
+                    target_chunk = torch.cat((target_chunk, tmp))
+                else:
+                    input_chunk = token_ids[i:i + max_length]
+                    target_chunk = token_ids[i + 1: i + max_length + 1]
+                # if max_length-input_chunk.shape[0]>0:
+                #     tmp = torch.full([max_length-input_chunk.shape[0]], tokenizer.eos_token_id)
+                #     input_chunk = torch.cat((input_chunk, tmp))
+                # if max_length-target_chunk.shape[0]>0:
+                #     tmp = torch.full([max_length-target_chunk.shape[0]], tokenizer.eos_token_id)
+                #     target_chunk = torch.cat((target_chunk, tmp))
+                # self.input_ids.append(tokenizer.decode(input_chunk))
+                # self.target_ids.append(target_chunk.detach().clone())
+                self.input_ids.append(input_chunk.detach().clone())
+                self.target_ids.append(target_chunk.detach().clone())
+                i = i+stride
+                remaining_size = remaining_size -stride
+
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.target_ids[idx]
