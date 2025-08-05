dc7ea7722686fd70d5a378805364d1e2b6d33687
jhe24 <jhe24@apple.com>
2025-08-05 11:32:46 +0800

    add temperature, top_k

--- MyGPTPretrainedModel.py
+++ MyGPTPretrainedModel.py
@@ -54,14 +54,31 @@ class MyGPTPretrainedModel(PreTrainedModel):
             loss = torch.nn.functional.cross_entropy(embedding.flatten(0, 1), labels.flatten(), ignore_index=self.config.pad_token_id)
         return {"loss": loss, "logits": embedding}
 
-def generate_text(model, idx, max_tokens:int, max_context:int):
+def generate_text(model, idx, max_tokens:int, max_context:int, temperature=0.0, top_k=None, eos_id=None):
     for _ in range(max_tokens):
         idx_cond = idx[-max_context:]
         with torch.no_grad():
             logits = model(idx_cond)['logits']
 
         logits = logits[-1, :]
-        probas = torch.softmax(logits, dim=-1)
-        idx_next = torch.argmax(probas, dim=-1, keepdim=True)
+
+        if top_k is not None:
+            top_logits, _ = torch.topk(logits, top_k)
+            min_val = top_logits[-1]
+            logits = torch.where(logits < min_val, torch.tensor(float("-inf")).to(logits.device), logits)
+
+        if temperature > 0.0:
+            logits = logits / temperature
+
+            # Apply softmax to get probabilities
+            probs = torch.softmax(logits, dim=-1)  # (batch_size, context_len)
+
+            # Sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1)  # (batch_size, 1)
+        else:
+            idx_next = torch.argmax(logits, dim=-1, keepdim=True)
+
+        if idx_next == eos_id:
+            break
         idx = torch.cat((idx, idx_next), dim=0)
     return idx
