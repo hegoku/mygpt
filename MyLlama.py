076df63cabfe82bfb95bfc362b60a4d5b66a8ada
jhe24 <jhe24@apple.com>
2025-08-29 17:13:09 +0800

    rmsnorm with float32

--- MyLlama.py
+++ MyLlama.py
@@ -85,3 +85,20 @@ def generate_text(model, idx, max_tokens:int, max_context:int):
         idx_next = torch.argmax(probas, dim=-1, keepdim=True)
         idx = torch.cat((idx, idx_next), dim=1)
     return idx
+
+class RMSNorm(nn.Module):
+    def __init__(self, dim: int, eps: float = 1e-6):
+        super().__init__()
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(dim))
+
+    def _norm(self, x):
+        return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
+
+    def forward(self, x):
+        output = self._norm(x.float())
+        output = output * (1.0 + self.weight.float())
+        return output.type_as(x)
+
+    def extra_repr(self):
+        return f"{tuple(self.weight.shape)}, eps={self.eps}"
